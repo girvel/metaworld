e6d4409f0aa55eacd4b9a9a7606a74b7c406fe86
girvel <widauka@ya.ru>
2022-02-28 22:18:19 +0500

    Add first place, rewrite the system to work with places too

--- metaworld.py
+++ metaworld.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from pathlib import Path
 
 import yaml
@@ -5,39 +6,77 @@ import yaml
 from lib.ecs.ecs import Metasystem, Entity, create_system
 
 
+class Action:
+    stands_at = namedtuple("stands", "place")
+    talks_to = namedtuple("talks", "person")
+
+
 if __name__ == '__main__':
     ms = Metasystem()
 
     @ms.add
     @create_system
-    def input_system(talker: 'talks_to'):
-        dialogue = talker.talks_to.lines[talker.talks_to.lines_state]
+    def action_system(actor: 'does'):
+        match actor.does:
+            case Action.talks_to(person):
+                dialogue = person.lines[person.lines_state]
 
-        for line in dialogue['lines']:
-            print(line, end='')
-            input()
+                for line in dialogue['lines']:
+                    print(line, end='')
+                    input()
 
-        if not 'options' in dialogue:
-            input("The end!")
-            exit()
+                if not 'options' in dialogue:
+                    input("The end!")
+                    exit()
 
-        for i, option in enumerate(dialogue['options']):
-            print(f'{i + 1}. {option["line"]}')
+                for i, option in enumerate(dialogue['options']):
+                    print(f'{i + 1}. {option["line"]}')
 
-        talker.talks_to.lines_state \
-            = dialogue['options'][int(input("> ")) - 1]['goto']
+                person.lines_state \
+                    = dialogue['options'][int(input("> ")) - 1]['goto']
+
+            case Action.stands_at(place):
+                for state in place.interior['states']:
+                    print(state['line'])
+
+                options = [
+                    option
+                    for state in place.interior['states']
+                    for option in state.get('options', [])
+                ]
+
+                for i, option in enumerate(options):
+                    print(f'{i + 1}. {option["line"]}')
+
+                name, state = options[int(input("> ")) - 1]['goto'].split('.')
+                person = next(
+                    p for p in place.persons if p.name.lower() == name.lower()
+                )
+
+                actor.does = Action.talks_to(person)
+                person.state = state
+
 
     brian = ms.add(Entity(
         name='Brian',
         lines=yaml.safe_load(
-            Path('assets/dialogues/brian.yaml').read_text(encoding='utf8')
+            Path('assets/characters/brian.yaml').read_text(encoding='utf8')
         ),
         lines_state='initial',
+    ))
+
+    pub = ms.add(Entity(
+        name='The pub',
+        interior=yaml.safe_load(
+            Path('assets/places/pub.yaml').read_text(encoding='utf8')
+        ),
+        persons=[brian],
     ))
 
     you = ms.add(Entity(
         name='Officer Aernerh',
         is_player='True',
+        does=Action.stands_at(pub),
         talks_to=brian,
     ))
 
