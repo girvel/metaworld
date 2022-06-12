36a6da38e088d406a8222511e2dcbab6dfb0b19b
girvel <widauka@ya.ru>
2022-06-12 17:36:23 +0400

    Remove obsolete notes

--- metaworld.py
+++ metaworld.py
@@ -4,21 +4,6 @@ from lib.ecs.ecs import Metasystem
 import systems
 
 
-# Concept:
-# systems/ contains all the systems
-# assets/ contains all the game's content
-# toolkit/ contains all the additional code used outside the systems
-# lib/ contains all the external libraries
-# metaworld.py is the entrypoint
-
-# TODO:
-# - unify option mechanics
-# - write illia pots's house
-# - make house states a dict as a npc's dialogues
-# - assert that names never match
-# - a book in the pub
-
-
 if __name__ == '__main__':
     ms = Metasystem()
 
